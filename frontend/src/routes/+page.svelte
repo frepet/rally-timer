596192c1241db65243be0cfe800b1e470148cc4e
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-25 21:12:06 +0200

    store to file

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { finishes } from '$lib/stores/finishes';
+	import { gatePassings } from '$lib/stores/passes';
 	import { startRfidWedge, type RfidStopFn } from '$lib/input/rfidWedge';
 
 	let url = 'http://localhost:8080/'; // your gate
@@ -13,8 +13,7 @@
 		es.onmessage = (msg) => {
 			try {
 				const data = JSON.parse(msg.data);
-				finishes.pushIso(data.ts_utc, data.id);
-				finishes.trim(500);
+				gatePassings.push(data.ts_utc, data.id);
 			} catch (err) {
 				console.error('Bad event:', err);
 			}
@@ -22,37 +21,34 @@
 		es.onerror = (err) => console.error('SSE error:', err);
 
 		// RFID wedge listener
-		stopRfid = startRfidWedge(
-			(tag) => {
-				// associate tag to the latest recent finish
-				finishes.associateTag(tag);
-			},
-			{
-				minLength: 3, // typical tags > 6 chars
-				idleResetMs: 500,
-				maxBurstMs: 3000,
-				terminators: ['Enter', 'Tab']
-				// allowed: /^[0-9]$/     // uncomment if your tags are digits-only
-			}
-		);
+		//stopRfid = startRfidWedge(
+		//	(tag) => {
+		//		// associate tag to the latest recent finish
+		//		finishes.associateTag(tag);
+		//	},
+		//	{
+		//		minLength: 3, // typical tags > 6 chars
+		//		idleResetMs: 500,
+		//		maxBurstMs: 3000,
+		//		terminators: ['Enter', 'Tab']
+		//		// allowed: /^[0-9]$/     // uncomment if your tags are digits-only
+		//	}
+		//);
 
-		return () => {
-			es.close();
-			if (stopRfid) stopRfid();
-		};
+		//return () => {
+		//	es.close();
+		//	if (stopRfid) stopRfid();
+		//};
 	});
 </script>
 
-<h1>Finish Event Queue</h1>
+<h1>Gate Passings</h1>
 
 <ol>
-	{#each $finishes as f}
+	{#each $gatePassings as f}
 		<li>
 			{f.gate_id}
 			{f.ts.toISOString()}
-			{#if f.id}
-				— ID: {f.id}
-			{/if}
 		</li>
 	{/each}
 </ol>
