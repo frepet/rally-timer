75a335d2b380f18fd07d869bbbb39ea193995942
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-27 22:09:26 +0200

    flowbite

--- rally-timer-app/src/routes/+page.svelte
+++ rally-timer-app/src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import { List, Card, P } from 'flowbite-svelte';
 	let { data }: PageProps = $props();
 </script>
 
-<h2>Passings</h2>
-<ul>
-	{#each data.passings as passing}
-		<li>Passing gate "{passing.gate_id}" @ {passing.timestamp} ms</li>
-	{/each}
-</ul>
+<Card class="p-4 sm:p-6 md:p-8">
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Passings</h5>
+	<List>
+		{#each data.passings as passing}
+			<P>
+				Passing gate "{passing.gate_id}" @ {passing.timestamp} ms
+			</P>
+		{/each}
+	</List>
+</Card>
