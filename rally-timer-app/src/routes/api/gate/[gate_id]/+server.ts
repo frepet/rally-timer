a0b573aa67b16b5f4ed4bb9cd6d07c6fdd85ff14
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-27 20:45:35 +0200

    push to database

--- rally-timer-app/src/routes/api/gate/[gate_id]/+server.ts
+++ rally-timer-app/src/routes/api/gate/[gate_id]/+server.ts
@@ -1,7 +1,21 @@
 import { json, type RequestEvent } from "@sveltejs/kit";
+import * as z from 'zod';
+import Database from "better-sqlite3";
+
+const Passing = z.object({
+  timestamp: z.number(),
+});
 
 export async function POST(event: RequestEvent): Promise<Response> {
-  console.log("test");
-  console.log(await event.request.json());
-  return json({ "debug": "hej" });
+  console.log("New POST");
+  const passing = Passing.parse(await event.request.json());
+  console.log(passing);
+  console.log(process.cwd());
+
+  const db = new Database('database.sqlite', { fileMustExist: true });
+  db.pragma('journal_mode = WAL');
+
+  let result = db.prepare("INSERT INTO passings(gate_id, timestamp) VALUES(?,?);").run(event.params.gate_id, passing.timestamp);
+
+  return json(result);
 }
