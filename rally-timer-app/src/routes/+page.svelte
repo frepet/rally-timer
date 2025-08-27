7bc3a96f5ca4b6daefe5579e92d054bc6d9d09b5
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-27 23:06:57 +0200

    send gate to rally-timer-app

--- rally-timer-app/src/routes/+page.svelte
+++ rally-timer-app/src/routes/+page.svelte
@@ -2,15 +2,28 @@
 	import type { PageProps } from './$types';
 	import { List, Card, P } from 'flowbite-svelte';
 	let { data }: PageProps = $props();
+	let passings = $state(data.passings);
+
+	$effect(() => {
+		const timeout = setInterval(async () => {
+			let result = await fetch('/api/gate');
+			passings = await result.json();
+		}, 1000);
+		return () => {
+			clearTimeout(timeout);
+		};
+	});
 </script>
 
-<Card class="p-4 sm:p-6 md:p-8">
-	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Passings</h5>
-	<List>
-		{#each data.passings as passing}
-			<P>
-				Passing gate "{passing.gate_id}" @ {passing.timestamp} ms
-			</P>
-		{/each}
-	</List>
-</Card>
+<div class="w-full p-5">
+	<Card class="max-w-none p-4 sm:p-6 md:p-8">
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Passings</h5>
+		<List>
+			{#each passings as passing}
+				<P>
+					- {passing.gate_id} @ {passing.timestamp} ms
+				</P>
+			{/each}
+		</List>
+	</Card>
+</div>
--- rally-timer-app/src/routes/api/gate/[gate_id]/+server.ts
+++ rally-timer-app/src/routes/api/gate/[gate_id]/+server.ts
@@ -3,14 +3,10 @@ import Database from "better-sqlite3";
 import { NewPassing } from "../../../../lib/types";
 
 export async function POST(event: RequestEvent): Promise<Response> {
-  console.log("New POST");
   const passing = NewPassing.parse(await event.request.json());
-  console.log(passing);
-  console.log(process.cwd());
 
   const db = new Database('database.sqlite', { fileMustExist: true });
   db.pragma('journal_mode = WAL');
-
   let result = db.prepare("INSERT INTO passings(gate_id, timestamp) VALUES(?,?) returning *;").get(event.params.gate_id, passing.timestamp);
 
   return json(result);
