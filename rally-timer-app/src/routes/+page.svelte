27e7a8233613b42c31d8b3a0165e919db4610078
Fredrik Peteri Harr <fredrik.peteri.harr@lumera.com>
2025-08-28 10:35:26 +0200

    delete rows in table

--- rally-timer-app/src/routes/+page.svelte
+++ rally-timer-app/src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-	import { List, Card, P, Button } from 'flowbite-svelte';
+	import {
+		Card,
+		Button,
+		Table,
+		TableHead,
+		TableHeadCell,
+		TableBody,
+		TableBodyRow,
+		TableBodyCell
+	} from 'flowbite-svelte';
 	import { TrashBinSolid } from 'flowbite-svelte-icons';
 	let { data }: PageProps = $props();
 	let passings = $state(data.passings);
@@ -24,14 +33,40 @@
 
 <div class="w-full p-5">
 	<Card class="max-w-none p-4 sm:p-6 md:p-8">
-		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Passings</h5>
-		<Button class="w-0" onclick={clearAllPassings}><TrashBinSolid /></Button>
-		<List>
-			{#each passings as passing}
-				<P>
-					- {passing.gate_id} @ {passing.timestamp} ms
-				</P>
-			{/each}
-		</List>
+		<div class="mb-2 flex">
+			<h5 class="mb-2 flex-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+				Passings
+			</h5>
+			<Button class="w-32" onclick={clearAllPassings}>Clear All</Button>
+		</div>
+		<Table hoverable={true}>
+			<TableHead>
+				<TableHeadCell>Gate</TableHeadCell>
+				<TableHeadCell>Timestamp</TableHeadCell>
+				<TableHeadCell class="flex justify-end">Actions</TableHeadCell>
+			</TableHead>
+			<TableBody>
+				{#each passings as passing}
+					<TableBodyRow>
+						<TableBodyCell>
+							{passing.gate_id}
+						</TableBodyCell>
+						<TableBodyCell>
+							{passing.timestamp}
+						</TableBodyCell>
+						<TableBodyCell class="flex justify-end">
+							<Button
+								size="xs"
+								onclick={async () => {
+									await fetch(`/api/gate/${passing.id}`, { method: 'DELETE' });
+									let result = await fetch('/api/gate');
+									passings = await result.json();
+								}}><TrashBinSolid /></Button
+							>
+						</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
 	</Card>
 </div>
