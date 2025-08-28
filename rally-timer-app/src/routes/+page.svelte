8351735be186d4f64bb325037e7993249c7d159c
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-28 08:55:26 +0200

    add icon on clear button

--- rally-timer-app/src/routes/+page.svelte
+++ rally-timer-app/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import { List, Card, P, Button } from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
 	let { data }: PageProps = $props();
 	let passings = $state(data.passings);
 
@@ -24,7 +25,7 @@
 <div class="w-full p-5">
 	<Card class="max-w-none p-4 sm:p-6 md:p-8">
 		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Passings</h5>
-		<Button class="w-0" onclick={clearAllPassings}>Clear</Button>
+		<Button class="w-0" onclick={clearAllPassings}><TrashBinSolid /></Button>
 		<List>
 			{#each passings as passing}
 				<P>
