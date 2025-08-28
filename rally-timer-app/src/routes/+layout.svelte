eb37cbb43c3d0c88b2a66135b5a026ab58254eae
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-28 20:26:25 +0200

    fix(rally-timer-app): move dark mode button inside hamburger menu

--- rally-timer-app/src/routes/+layout.svelte
+++ rally-timer-app/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+	import { DarkMode, Heading, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
 
 	let { children } = $props();
 </script>
@@ -11,17 +11,16 @@
 </svelte:head>
 <Navbar>
 	<NavBrand href="/">
-		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-			>Rally Timer</span
-		>
+		<Heading>Rally Timer</Heading>
 	</NavBrand>
-	<div class="flex justify-end">
-		<DarkMode />
-	</div>
+
 	<NavHamburger />
 	<NavUl>
 		<NavLi href="/">Home</NavLi>
 		<NavLi href="/gate">Gate Events</NavLi>
+		<NavLi>
+			<DarkMode class="flex-1" />
+		</NavLi>
 	</NavUl>
 </Navbar>
 {@render children?.()}
