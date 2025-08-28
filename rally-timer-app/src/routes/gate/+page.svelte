659b3cac9e877e09bebd048d30b2601a0e219348
Fredrik Peteri Harr <fredrik@peteri.se>
2025-08-28 11:20:08 +0200

    add: navbar

---
+++ rally-timer-app/src/routes/gate/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+	import {
+		Card,
+		Button,
+		Table,
+		TableHead,
+		TableHeadCell,
+		TableBody,
+		TableBodyRow,
+		TableBodyCell
+	} from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
+	let { data }: PageProps = $props();
+	let gateEvents = $state(data.gateEvents);
+
+	$effect(() => {
+		const timeout = setInterval(async () => {
+			let result = await fetch('/api/gate');
+			gateEvents = await result.json();
+		}, 1000);
+		return () => {
+			clearTimeout(timeout);
+		};
+	});
+
+	async function clearAllPassings() {
+		await fetch('/api/gate', { method: 'DELETE' });
+		let result = await fetch('/api/gate');
+		gateEvents = await result.json();
+	}
+</script>
+
+<div class="w-full p-5">
+	<Card class="max-w-none p-4 sm:p-6 md:p-8">
+		<div class="mb-2 flex">
+			<h5 class="mb-2 flex-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+				Gate Events
+			</h5>
+			<Button class="w-32" onclick={clearAllPassings}>Clear All</Button>
+		</div>
+		<Table hoverable={true}>
+			<TableHead>
+				<TableHeadCell>Gate</TableHeadCell>
+				<TableHeadCell>Timestamp</TableHeadCell>
+				<TableHeadCell class="flex justify-end">Actions</TableHeadCell>
+			</TableHead>
+			<TableBody>
+				{#each gateEvents as e}
+					<TableBodyRow>
+						<TableBodyCell>
+							{e.gate_id}
+						</TableBodyCell>
+						<TableBodyCell>
+							{e.timestamp}
+						</TableBodyCell>
+						<TableBodyCell class="flex justify-end">
+							<Button
+								size="xs"
+								onclick={async () => {
+									await fetch(`/api/gate/${e.id}`, { method: 'DELETE' });
+									let result = await fetch('/api/gate');
+									gateEvents = await result.json();
+								}}><TrashBinSolid /></Button
+							>
+						</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
+	</Card>
+</div>
