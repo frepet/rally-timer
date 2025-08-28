f84c55f0fd09c154bdac6cda28dfc4c01a5a86fa
Fredrik Peteri Harr <fredrik.peteri.harr@lumera.com>
2025-08-28 08:26:15 +0200

    add clear button

--- rally-timer-app/src/routes/+page.svelte
+++ rally-timer-app/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-	import { List, Card, P } from 'flowbite-svelte';
+	import { List, Card, P, Button } from 'flowbite-svelte';
 	let { data }: PageProps = $props();
 	let passings = $state(data.passings);
 
@@ -13,11 +13,18 @@
 			clearTimeout(timeout);
 		};
 	});
+
+	async function clearAllPassings() {
+		await fetch('/api/gate', { method: 'DELETE' });
+		let result = await fetch('/api/gate');
+		passings = await result.json();
+	}
 </script>
 
 <div class="w-full p-5">
 	<Card class="max-w-none p-4 sm:p-6 md:p-8">
 		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Passings</h5>
+		<Button class="w-0" onclick={clearAllPassings}>Clear</Button>
 		<List>
 			{#each passings as passing}
 				<P>
--- rally-timer-app/src/routes/api/gate/+server.ts
+++ rally-timer-app/src/routes/api/gate/+server.ts
@@ -1,10 +1,18 @@
 import { json, type RequestEvent } from "@sveltejs/kit";
 import Database from "better-sqlite3";
 
+const db = new Database('database.sqlite', { fileMustExist: true });
+
 export async function GET(event: RequestEvent): Promise<Response> {
-  const db = new Database('database.sqlite', { fileMustExist: true });
   db.pragma('journal_mode = WAL');
   let passings = db.prepare("SELECT * FROM passings ORDER BY(timestamp)").all();
 
   return json(passings);
+}
+
+export async function DELETE(event: RequestEvent): Promise<Response> {
+  db.pragma('journal_mode = WAL');
+  db.exec("DELETE FROM passings");
+
+  return new Response(null, { status: 204 });
 }
